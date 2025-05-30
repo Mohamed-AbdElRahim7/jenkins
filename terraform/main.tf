bc6bc3d1eeff150b89e74b706245eaf3847af858
Mohamed Abd El_Rahim <161028740+Mohamed-AbdElRahim7@users.noreply.github.com>
2025-05-30 12:24:58 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,25 +1,66 @@
+# Define the AWS provider and region.
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-1" # Hardcoded for clarity, can be passed via variable if needed
 }
 
-resource "aws_instance" "web" {
-  ami           = "ami-0f88e80871fd81e91"
-  instance_type = "t2.micro"
-  key_name      = var.key_name
+# Define a security group to allow SSH access.
+resource "aws_security_group" "ssh_sg" {
+  name        = "jenkins-ssh-sg"
+  description = "Allow SSH inbound traffic for Jenkins pipeline"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] // WARNING: Restrict this in production!
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = {
-    Name = "jenkins-ec2"
+    Name = "jenkins-ssh-sg"
   }
 }
 
-output "instance_ip" {
-  value = aws_instance.web.public_ip
+# Data source to find the most recent Ubuntu Server 22.04 LTS AMI.
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"] # Canonical's owner ID
 }
 
+# Data source to get the default VPC.
+data "aws_vpc" "default" {
+  default = true
+}
 
-### terraform/variables.tf
+# Define the EC2 instance.
+resource "aws_instance" "jenkins_ec2" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  key_name      = "jenkins"  # IMPORTANT: This refers to the name of the EC2 Key Pair
+  vpc_security_group_ids = [aws_security_group.ssh_sg.id]
 
-variable "key_name" {
-  description = "The key name to use for the EC2 instance"
-  type        = string
+  tags = {
+    Name = "JenkinsManagedEC2"
+  }
+}
+
+# Output the public IP address of the EC2 instance.
+output "public_ip" {
+  value       = aws_instance.jenkins_ec2.public_ip
+  description = "The public IP address of the EC2 instance."
 }
